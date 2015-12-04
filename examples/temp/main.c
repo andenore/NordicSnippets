964c4e6cbf36533cc79e4045f5ce877d05baf796
Ronan Barzic <rbarzic@gmail.com>
2015-12-04 13:14:49 +0100

    Clean-up warnings - Hide commands from Makefile outputs

--- examples/temp/main.c
+++ examples/temp/main.c
@@ -3,24 +3,27 @@
 int main(void)
 {
   volatile uint32_t temperature;
-  
+
   // Start HFCLK crystal oscillator, since that will give highest accuracy
   NRF_CLOCK->TASKS_HFCLKSTART = 1;
   while (NRF_CLOCK->EVENTS_HFCLKSTARTED == 0);
   NRF_CLOCK->EVENTS_HFCLKSTARTED = 0;
-  
+
   // Trigger temperature measurement
   NRF_TEMP->TASKS_START = 1;
-  
+
   // Wait until measurement is finished
   while (NRF_TEMP->EVENTS_DATARDY == 0);
   NRF_TEMP->EVENTS_DATARDY = 0;
-  
+
   // Read temperature and convert to celcius (rounding down)
   temperature = (int32_t)NRF_TEMP->TEMP / 4;
-    
+
+  temperature; // to get rid of "set but not used" warning
+
   while (1)
   {
     __WFE();
   }
+
 }
