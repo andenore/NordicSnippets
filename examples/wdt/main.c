dee2712209f20ea74085e1eca48714f55d826c77
Dietz, Michael <Michael.Dietz@nordicsemi.no>
2016-01-15 10:24:16 +0100

    WDT example working

--- examples/wdt/main.c
+++ examples/wdt/main.c
@@ -1,28 +1,24 @@
 #include <nrf.h>
-
-#define PIN_GPIO  (17UL)
+#include <stdint.h>
 
 int main(void)
 {
-  // Configure GPIO pin as output with standard drive strength.
-  NRF_GPIO->PIN_CNF[PIN_GPIO] = (GPIO_PIN_CNF_DIR_Output << GPIO_PIN_CNF_DIR_Pos) |
-                                (GPIO_PIN_CNF_DRIVE_S0S1 << GPIO_PIN_CNF_DRIVE_Pos) |
-                                (GPIO_PIN_CNF_INPUT_Connect << GPIO_PIN_CNF_INPUT_Pos) |
-                                (GPIO_PIN_CNF_PULL_Disabled << GPIO_PIN_CNF_PULL_Pos) |
-                                (GPIO_PIN_CNF_SENSE_Disabled << GPIO_PIN_CNF_SENSE_Pos);
-  
-  
-  // Toggle GPIO ON/OFF.
-  while (1)
-  {
-    uint32_t volatile tmo;
-    
-    tmo = 10000000;
-    while (tmo--);
-    NRF_GPIO->OUTSET = (1UL << PIN_GPIO);    
-    
-    tmo = 10000000;
-    while (tmo--);
-    NRF_GPIO->OUTCLR = (1UL << PIN_GPIO);  
-  }
+	const static uint32_t WDT_BASE_ADDRESS = 0x40010000ul;
+	const static uint32_t WDT_TASKS_START  = 0x000;
+	const static uint32_t WDT_CRV          = 0x504;
+	const static uint32_t WDT_CONFIG       = 0x50C;
+	const static uint32_t WDT_RREN         = 0x508;
+	const static uint32_t WDT_RR0          = 0x600;
+	
+	*((uint32_t *) (WDT_BASE_ADDRESS | WDT_CRV))         = 0x0Ful; // timeout = (CRV+1) / 32768. This is the shortest timeout possible.
+	// *((uint32_t *) (WDT_BASE_ADDRESS | WDT_CONFIG))      = 0x09ul; // Configure the WDT to keep counting down even when the CPU is halted by the debugger.
+	*((uint32_t *) (WDT_BASE_ADDRESS | WDT_RREN))        = 0x01ul; // Enable reload register 0 to feed the dog.
+	
+	*((uint32_t *) (WDT_BASE_ADDRESS | WDT_TASKS_START)) = 0x01ul; // WDT will automatically start the LFCLK source if it is not already being generated.
+	
+	for(;;)
+	{
+		// infinite loop.
+		*((uint32_t *) (WDT_BASE_ADDRESS | WDT_RR0))       = 0x6E524635; // Kick the dog to prevent it from issuing a watch dog reset.
+	}
 }
