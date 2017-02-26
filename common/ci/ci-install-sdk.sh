a6b152384fed71af8ea234cb18ec12bf070ccbc5
Anders Nore <anders.nore@nordicsemi.no>
2017-02-26 21:51:50 +0100

    Updated SDK

--- common/ci/ci-install-sdk.sh
+++ common/ci/ci-install-sdk.sh
@@ -2,8 +2,8 @@
 # Install SDK for Travis-CI
 set -ex
 SDK_INSTALL_DIR=/tmp
-SDK_URL=http://developer.nordicsemi.com/nRF52_SDK/nRF52_SDK_v0.x.x
-SDK_VERSION=nRF52_SDK_0.9.2_dbc28c9
+SDK_URL=http://developer.nordicsemi.com/nRF5_SDK/nRF5_SDK_v12.x.x
+SDK_VERSION=nRF5_SDK_12.2.0_f012efa
 # Remove old file if exists
 rm -Rf /tmp/$SDK_VERSION.zip
 rm -Rf /tmp/nrf52-sdk-for-travis-ci
